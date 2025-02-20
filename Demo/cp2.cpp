7d252739bbf6c1e3af8eb59097bbb06108de29de
CHARAN REDDY <charanreddy.puchala@gmail.com>
2025-02-20 22:37:33 +0530

    Create cp2.cpp

---
+++ Demo/cp2.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+template <typename T>
+istream &operator>>(istream &istream, vector<T> &v) {
+    for (auto &it : v) istream >> it;
+    return istream;
+}
+
+template <typename T>
+ostream &operator<<(ostream &ostream, const vector<T> &v) {
+    for (const auto &it : v) ostream << it << ' ';
+    return ostream;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int tt;
+    cin >> tt;
+    while(tt--){
+
+    }
+    return 0;
+}
